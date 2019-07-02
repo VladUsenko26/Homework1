cd8ecf5536636ba0f4ccd7abeadbec35297a7aae
Vlad <usenkovlad94@gmail.com>
2019-07-02 12:42:06 +0300

    All cells have own timer

--- Homework_1/ViewControllers/Benchmark/BenchmarkViewController.swift
+++ Homework_1/ViewControllers/Benchmark/BenchmarkViewController.swift
@@ -13,52 +13,34 @@ class BenchmarkViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    private var timerBehavior: TimerBehavior!
-    private var timerValue: String?
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionView.register(BenchmarkCollectionViewCell.cellNib, forCellWithReuseIdentifier: BenchmarkCollectionViewCell.id)
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
-        self.timerBehavior = TimerBehavior()
-        self.timerBehavior.delegate = self
-        addBehaviors(behaviors: [self.timerBehavior])
+       
+        addBehaviors(behaviors: [TimerBehavior()])
     }
     
 }
 
 extension BenchmarkViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return 2
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BenchmarkCollectionViewCell.id, for: indexPath) as! BenchmarkCollectionViewCell
-        if let timer = timerValue {
-            cell.timerLabel_1.text = timer
-            cell.timerLabel_2.text = timer
-        }
+
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        self.timerBehavior.pauseTimer()
+        let cell = collectionView.cellForItem(at: indexPath) as? BenchmarkCollectionViewCell
+        if let cell = cell {
+            cell.pauseTimer()
+        }
     }
     
-}
-
-extension BenchmarkViewController : TimerBehaviorDelegate {
-    
-    func newTime(date: Date) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEE, dd MMM yyyy hh:mm:ss +zzzz"
-        let dateString = dateFormatter.string(from: date)
-        timerValue = dateString
-        collectionView.reloadData()
-    }
-    func timerInvalidated() {
-        timerValue = "Timer Invalidated"
-        collectionView.reloadData()
-    }
 }
 
--- Homework_1/ViewControllers/Benchmark/CollectionViewCells/BenchmarkCollectionViewCell.swift
+++ Homework_1/ViewControllers/Benchmark/CollectionViewCells/BenchmarkCollectionViewCell.swift
@@ -13,11 +13,43 @@ class BenchmarkCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var timerLabel_1: UILabel!
     @IBOutlet weak var timerLabel_2: UILabel!
     
+    private var timer: Timer?
+    private var isPaused: Bool = false
+    
     static var id: String {
         return String(describing: self)
     }
     
     static var cellNib: UINib {
         return UINib(nibName: id, bundle: nil)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        if !isPaused {
+            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(runTimed), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @objc private func runTimed() {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEE, dd MMM yyyy hh:mm:ss +zzzz"
+        let dateString = dateFormatter.string(from: Date())
+        self.timerLabel_1.text = dateString
+        self.timerLabel_2.text = dateString
+    }
+    
+    func pauseTimer () {
+        if isPaused{
+            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(runTimed), userInfo: nil, repeats: true)
+            isPaused = false
+        } else {
+            guard let timer = self.timer else {
+                return
+            }
+            timer.invalidate()
+            isPaused = true
+        }
     }
 }
